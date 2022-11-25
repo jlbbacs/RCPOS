ee5c546f174e2c65b7bd93d8b414a3403b5072a0
jlbbacs <josephlestebacs@gmail.com>
2022-11-25 14:13:37 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from 'react-router-dom';
+
+import Navbar from "./components/Navigation";
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
+import Features from "./pages/Features";
+import OurClients from "./pages/OurClients";
+import ContactUs from './pages/ContactUs';
+
+
+
+
+
+const App = () => {
+
+  return (
+
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="Features" element={<Features/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contactus" element={<ContactUs/>} />
+        <Route path="/outclients" element={<OurClients/>} />
+
+    </Routes>
+    </Router>
+  
+  )
+}
+
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+const Home = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Home;
