846e99b58e743579cde1c72ba847dd11f51dd24f
jlbbacs <josephlestebacs@gmail.com>
2022-11-26 13:22:47 +0800

    fixing responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
       <div class="card mb-3 mt-5">
       <img src="https://www.investsmall.co/wp-content/uploads/2021/09/POS-agencies-in-Nigeria.jpg"  class="card-img-top "  alt="..." />
       <div class="card-body" >
-        <h5 class="card-title fw-bold mt-3"  >RCPOS PROVIDER, from order placement to payment and everything in between.</h5>
+        <h4 class="card-title fw-bold mt-3"  >RCPOS PROVIDER, from order placement to payment and everything in between.</h4>
         <p class="card-text mt-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
       </div>
       </div>
@@ -33,11 +33,11 @@ const Home = () => {
 
     <div class="card mb-3 mt-5" style={{maxwidth:540}}>
   <div class="row g-0">
-    <div class="col-md-6" data-aos="fade-right" data-aos-duration="1000" >
+    <div class="col-md-6"  >
       <img src="https://fitsmallbusiness.com/wp-content/uploads/2022/08/FeatureImage_Types_of_POS_Systems.jpg" class="img-fluid rounded shadow-lg"  alt="..."/>
     </div>
     <div class="col-md-6 ">
-      <div class="card-body mt-5" data-aos="fade-left" data-aos-duration="1000">
+      <div class="card-body mt-5" >
         <h5 class="card-title fw-bold ">Get Paid Faster</h5>
         <p class="card-text">Never lose a sale because of the limited payment options. With Tagrain’s ability to collect payments in cash, cards, loyalty points, and gift cards, make sure your customer has got enough options to pay you.</p>
         <br></br>
@@ -53,13 +53,13 @@ const Home = () => {
   <div class="row g-0">
    
     <div class="col-md-6 " >
-      <div class="card-body mt-5" data-aos="fade-right" data-aos-duration="1000">
+      <div class="card-body mt-5" >
         <h5 class="card-title fw-bold " >Flexible Pricing</h5>
         <p class="card-text">Never lose a sale because of the limited payment options. With Tagrain’s ability to collect payments in cash, cards, loyalty points, and gift cards, make sure your customer has got enough options to pay you.</p>
         
       </div>
     </div>
-    <div class="col-md-6 " data-aos="fade-left" data-aos-duration="1000">
+    <div class="col-md-6 " >
       <img src="https://img.freepik.com/premium-photo/human-hand-working-with-3d-rendering-cashier-machine_493806-1446.jpg?w=2000" class="img-fluid rounded shadow-lg" alt="..."/>
     </div>
 
