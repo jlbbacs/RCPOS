40834f34f42612084ae8a97382eb17b8f6c5d56a
jlbbacs <josephlestebacs@gmail.com>
2022-11-28 12:15:58 +0800

    removing mission

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -16,7 +16,7 @@ const AboutUs = () => {
 <div class="card mb-3 mt-5">
   <img src="https://www.goodbody.ie/images/default-source/blogs/investments/2021-investments/february-investments-2021/market-pulse-3-investments-goodbody-wide-feb21.jpg?sfvrsn=73bf9fd5_2" class="card-img-top" alt="..."/>
   
-  <div class="container">
+  <div class="container mt-5">
 
   <div class="card-body">
     <h4 class="card-title">What we do</h4>
@@ -24,13 +24,7 @@ const AboutUs = () => {
    <p class="card-text">At Tagrain we set our ambition to have a positive impact on the lives of millions of small-scale retailers by increasing their income through operational efficiency.</p>
   </div>
 
-  <div class="card text-white mb-3" id="mission-card" style={{maxwidth:100}}>
-  <div class="card-header">Our Mission</div>
-  <div class="card-body">
-   
-    <p class="card-text text-dark">Empower small-scale retailers for growth by offering an easy to use and scalable point-of-sale solution.</p>
-  </div>
-</div>
+ 
   
 </div>
 
