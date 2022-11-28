8759de3cf89db9a63b8900ae831c7919ca61fd8d
jlbbacs <josephlestebacs@gmail.com>
2022-11-28 12:59:07 +0800

    fixing margin

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -16,9 +16,9 @@ const AboutUs = () => {
 <div class="card mb-3 mt-5">
   <img src="https://www.goodbody.ie/images/default-source/blogs/investments/2021-investments/february-investments-2021/market-pulse-3-investments-goodbody-wide-feb21.jpg?sfvrsn=73bf9fd5_2" class="card-img-top" alt="..."/>
   
-  <div class="container mt-5">
+  <div class="container">
 
-  <div class="card-body">
+  <div class="card-body" id="about-what-text">
     <h4 class="card-title fw-bold">What we do</h4>
     <p class="card-text ">RCPOS innovative point of sale solution drives customer acquisition, increase average order size, and makes the dream of long-lasting, loyal customer relationships a reality.</p>
    <p class="card-text">At Tagrain we set our ambition to have a positive impact on the lives of millions of small-scale retailers by increasing their income through operational efficiency.</p>
